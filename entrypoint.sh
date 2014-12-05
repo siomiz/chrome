c1c97b91bfa615262d16af9bc3e8801bf70b99da
Tomohisa Kusano <siomiz@gmail.com>
2014-12-06 04:52:59 +0900

    use xdotool to make the window fill screen

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@ set -e
 
 Xvfb :1 -screen 0 1024x768x24 &
 DISPLAY=:1 /opt/google/chrome/chrome --user-data-dir --no-sandbox &
+DISPLAY=:1 xdotool search --sync --onlyvisible --class "chrome" windowsize 1024 768
 
 exec "$@"
