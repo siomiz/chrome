2601fdf8a06fd2334c088bb73612f1f97799d7a2
npr0n <npronporn@gmail.com>
2021-01-26 19:57:25 +0100

    fixed subprocess.run with shell=True

--- copyables/home/chrome/bookmark.py
+++ copyables/home/chrome/bookmark.py
@@ -6,4 +6,4 @@ out_luscious = "/output/luscious"
 for url in chrome_bookmarks.urls:
     if "https://www.luscious.net/" in url.url:
         cmd = "lsd -o "+out_luscious+" -a "+url.url
-        subprocess.run(cmd)
+        subprocess.run(cmd, shell=True)
