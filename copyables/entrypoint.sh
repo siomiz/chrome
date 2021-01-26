375aa4d4f92219a7570451fe39378305354c6b3b
npr0n <npronporn@gmail.com>
2021-01-26 19:10:49 +0100

    fixed chown in entrypoint

--- copyables/entrypoint.sh
+++ copyables/entrypoint.sh
@@ -7,8 +7,8 @@ PGID=${PGID:-911}
 groupmod -o -g "$PGID" chrome
 usermod -o -u "$PUID" chrome
 
-chown -R /output
-chown -R /home/chrome
+chown -R /output chrome:chrome
+chown -R /home/chrome chrome:chrome
 
 # VNC default no password
 export X11VNC_AUTH="-nopw"
